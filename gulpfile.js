41eafab25d85487de5cb4c720295ca7f5051c09c
Phillip M. Hoff <phillip@orst.edu>
2015-12-10 22:39:48 -0800

    Initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var args = require('yargs').argv;
+var gulp = require('gulp');
+var jeditor = require('gulp-json-editor');
+var merge = require('merge-stream');
+var zip = require('gulp-zip');
+
+gulp.task('build', function() {
+	var manifest = gulp.src('manifest.json', { base: './' })
+		.pipe(jeditor(
+			function(json)
+			{
+				json.resources[0].url = args.url;
+				
+				return json;
+			}));	
+	
+	var assets = gulp.src('icons/*', { base: './' });
+	
+	return merge(manifest, assets)
+		.pipe(zip('medical.webtile'))
+		.pipe(gulp.dest('out'));
+});
+
+gulp.task('default', [ 'build' ]);
